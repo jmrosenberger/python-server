2dcc8a6beadb7e8f501c24af11150cf3fb75e6ab
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-10-15 09:55:05 -0500

    completed chapter 7, added class constructors

---
+++ models/employee.py
@@ -0,0 +1,10 @@
+class Employee():
+    """Class constructor to create a new instance of Employee
+    """
+    def __init__(self, id, name, email, phone, location_id):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.location_id = location_id
+        
