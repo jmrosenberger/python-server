acb0d7d660a014ddfd309d9f0fce0797dbecb4dd
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-10-19 13:53:31 -0500

    completed chapter 9

--- models/employee.py
+++ models/employee.py
@@ -1,10 +1,9 @@
 class Employee():
     """Class constructor to create a new instance of Employee
     """
-    def __init__(self, id, name, email, phone, location_id):
+    def __init__(self, id, name, address, location_id):
         self.id = id
         self.name = name
-        self.email = email
-        self.phone = phone
+        self.address = address
         self.location_id = location_id
         
--- models/location.py
+++ models/location.py
@@ -2,8 +2,7 @@ class Location():
     """Class constructor to create a new instance of Location
     """
 
-    def __init__(self, id, name, address, status):
+    def __init__(self, id, name, address):
         self.id = id
         self.name = name
         self.address = address
-        self. status = status
